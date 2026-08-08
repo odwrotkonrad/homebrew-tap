df0741149da5a9364d30882e80a77c8297470c2a
go-modules-brew-publisher <project_85188856_bot_5aca6b5f3d0fe8d2c7a0a9e6beb9ff4f@noreply.gitlab.com>
2026-08-08 18:14:24 +0000

    chore: che 0.0.69

--- Formula/che.rb
+++ Formula/che.rb
@@ -1,27 +1,27 @@
 class Che < Formula
   desc "Spec-driven dotfile configuration loader"
   homepage "https://gitlab.com/konradodwrot/go-modules"
-  version "0.0.68"
+  version "0.0.69"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.68/che_0.0.68_darwin_arm64.tar.gz"
-      sha256 "3993a98de001e1ea014f762b18c169d8675a4374ba20ef69526cfab1a7d5476b"
+      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.69/che_0.0.69_darwin_arm64.tar.gz"
+      sha256 "96f3889d2cbbddf911012ce92c5598be35971ce6ffac121a3c7074fb539e99b9"
     end
     on_intel do
-      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.68/che_0.0.68_darwin_amd64.tar.gz"
-      sha256 "9e84f7635dbd7c3a1b9b12c3d85fe6ec5513f32c3fcbc5e2828bddb3e1f58cb7"
+      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.69/che_0.0.69_darwin_amd64.tar.gz"
+      sha256 "d0b14befb33b17ac56bd1dca8838a2806deb90ee64fc3b41865cd5cc5fac981d"
     end
   end
   on_linux do
     on_arm do
-      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.68/che_0.0.68_linux_arm64.tar.gz"
-      sha256 "3b4001be964eea54333d87574a046bb75193193f675c812bd02ede6db5219987"
+      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.69/che_0.0.69_linux_arm64.tar.gz"
+      sha256 "e8e14b745e558c42792a59d5e680d6586387e2fb076555e0cb7295cea4fd27ef"
     end
     on_intel do
-      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.68/che_0.0.68_linux_amd64.tar.gz"
-      sha256 "bcef884ce9e3c3ad2dd338563569f07d7f2a9357a378886967b87465ccdee0a6"
+      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.69/che_0.0.69_linux_amd64.tar.gz"
+      sha256 "ce2dfb98e62e4dd37f9c60f5b4153f111b180672c949c84bb27114d705469b56"
     end
   end
 
