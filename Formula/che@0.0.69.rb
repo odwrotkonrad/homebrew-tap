8f5d90c2e6e99c1ed18a2ce12a176bf4e6356197
go-modules-brew-publisher <project_85188856_bot_5aca6b5f3d0fe8d2c7a0a9e6beb9ff4f@noreply.gitlab.com>
2026-08-08 18:14:25 +0000

    chore: che 0.0.69

---
+++ Formula/che@0.0.69.rb
@@ -0,0 +1,35 @@
+class CheAT0069 < Formula
+  desc "Spec-driven dotfile configuration loader"
+  homepage "https://gitlab.com/konradodwrot/go-modules"
+  version "0.0.69"
+  license "MIT"
+
+  on_macos do
+    on_arm do
+      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.69/che_0.0.69_darwin_arm64.tar.gz"
+      sha256 "96f3889d2cbbddf911012ce92c5598be35971ce6ffac121a3c7074fb539e99b9"
+    end
+    on_intel do
+      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.69/che_0.0.69_darwin_amd64.tar.gz"
+      sha256 "d0b14befb33b17ac56bd1dca8838a2806deb90ee64fc3b41865cd5cc5fac981d"
+    end
+  end
+  on_linux do
+    on_arm do
+      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.69/che_0.0.69_linux_arm64.tar.gz"
+      sha256 "e8e14b745e558c42792a59d5e680d6586387e2fb076555e0cb7295cea4fd27ef"
+    end
+    on_intel do
+      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.69/che_0.0.69_linux_amd64.tar.gz"
+      sha256 "ce2dfb98e62e4dd37f9c60f5b4153f111b180672c949c84bb27114d705469b56"
+    end
+  end
+
+  def install
+    bin.install "che", "render-tpl", "render-dirs-tree", "render-makefile-doc", "render-repo-group-index"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/che --version")
+  end
+end
