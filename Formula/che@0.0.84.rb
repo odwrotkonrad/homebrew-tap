e23a5a2c8ac1adb61655a3ae61db6a09a4461874
go-modules-brew-publisher <project_85188856_bot_5aca6b5f3d0fe8d2c7a0a9e6beb9ff4f@noreply.gitlab.com>
2026-08-19 16:30:04 +0000

    chore: che 0.0.84

---
+++ Formula/che@0.0.84.rb
@@ -0,0 +1,31 @@
+class CheAT0084 < Formula
+  desc "Spec-driven dotfile configuration loader"
+  homepage "https://gitlab.com/konradodwrot/go-modules"
+  version "0.0.84"
+  license "MIT"
+
+  on_macos do
+    on_arm do
+      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.84/che_0.0.84_darwin_arm64.tar.gz"
+      sha256 "2a316f37834f2b1f812f8179a9d03a9fb9aa38919b679575cfcce9098558e2a3"
+    end
+  end
+  on_linux do
+    on_arm do
+      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.84/che_0.0.84_linux_arm64.tar.gz"
+      sha256 "b6bcc220ac3f5ecd7cd4cade0f21dd00c0b6f77ce75f4be69327e602029675ac"
+    end
+    on_intel do
+      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.84/che_0.0.84_linux_amd64.tar.gz"
+      sha256 "8e1e4b922315c2adbaa875b6eb4b64a3e96f39de9e0007c0a39fbbdb5a1391e9"
+    end
+  end
+
+  def install
+    bin.install "che"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/che --version")
+  end
+end
