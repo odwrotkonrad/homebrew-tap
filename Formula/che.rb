b51ec4c7aefda6412b4583997cbc3e0cdb577362
go-modules-brew-publisher <project_85188856_bot_b4e64a0e228dbaed8c7170bf97b2eccf@noreply.gitlab.com>
2026-08-28 20:32:36 +0000

    chore: che 0.0.102

--- Formula/che.rb
+++ Formula/che.rb
@@ -1,23 +1,23 @@
 class Che < Formula
   desc "Spec-driven dotfile configuration loader"
   homepage "https://gitlab.com/konradodwrot/go-modules"
-  version "0.0.101"
+  version "0.0.102"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.101/che_0.0.101_darwin_arm64.tar.gz"
-      sha256 "5d50dba3066a054a286e0110efed8f248fd28d992a57866ccb268838924fce86"
+      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.102/che_0.0.102_darwin_arm64.tar.gz"
+      sha256 "f656348de68297b10206293e6ce6cea14b9ad019ecf8afa1eee21699ab63b5e8"
     end
   end
   on_linux do
     on_arm do
-      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.101/che_0.0.101_linux_arm64.tar.gz"
-      sha256 "891ebc0909e00fee0233b685ef4ec04471902933eecce8fec7387b4b89319e1d"
+      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.102/che_0.0.102_linux_arm64.tar.gz"
+      sha256 "756d9cdd09802b89878e5cb4208bc19fdce820b3e574b7837f23eae34590b3e2"
     end
     on_intel do
-      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.101/che_0.0.101_linux_amd64.tar.gz"
-      sha256 "474d8ba1d53a5a080953fadecd9ef5c1519ca7dd8a601afe06048e67a5c3d40a"
+      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.102/che_0.0.102_linux_amd64.tar.gz"
+      sha256 "ba90ed3e06e07637b44e25fc0dadbc16de3c293845ce32c4fe63d2cf60964ab4"
     end
   end
 
