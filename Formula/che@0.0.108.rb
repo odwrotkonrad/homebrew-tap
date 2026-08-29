90a86795013021c5e6599e8635611e1a9c95ce1a
go-modules-brew-publisher <project_85188856_bot_b4e64a0e228dbaed8c7170bf97b2eccf@noreply.gitlab.com>
2026-08-29 17:17:02 +0000

    chore: che 0.0.108

---
+++ Formula/che@0.0.108.rb
@@ -0,0 +1,31 @@
+class CheAT00108 < Formula
+  desc "Spec-driven dotfile configuration loader"
+  homepage "https://gitlab.com/konradodwrot/go-modules"
+  version "0.0.108"
+  license "MIT"
+
+  on_macos do
+    on_arm do
+      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.108/che_0.0.108_darwin_arm64.tar.gz"
+      sha256 "e117fef1a1569a0bc9cf112f956dfac0dc9c8a358b503f9bf207492bf1e5c0fe"
+    end
+  end
+  on_linux do
+    on_arm do
+      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.108/che_0.0.108_linux_arm64.tar.gz"
+      sha256 "27b587ec0c4c14fd9329828d075295e9526785a06e1b69564de22f51598049d1"
+    end
+    on_intel do
+      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.108/che_0.0.108_linux_amd64.tar.gz"
+      sha256 "fceb4bcb302db5e9d663d128686714f92b7a53edd84c448d3c74be92ab9c08bf"
+    end
+  end
+
+  def install
+    bin.install "che"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/che --version")
+  end
+end
