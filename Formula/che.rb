60ec35836399cc40bdd590b911ba1fb77410ca30
go-modules-brew-publisher <project_85188856_bot_b4e64a0e228dbaed8c7170bf97b2eccf@noreply.gitlab.com>
2026-09-06 18:56:11 +0000

    chore: che 0.0.127

--- Formula/che.rb
+++ Formula/che.rb
@@ -1,23 +1,23 @@
 class Che < Formula
   desc "Spec-driven dotfile configuration loader"
-  homepage "https://gitlab.com/konradodwrot/go-modules"
-  version "0.0.123"
+  homepage "https://gitlab.com/konradodwrot/private/che/che"
+  version "0.0.127"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.123/che_0.0.123_darwin_arm64.tar.gz"
-      sha256 "a98c84e53084e9e9c72144c4afd2252096836b75873d1c6fd309e5bf04014ab3"
+      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.127/che_0.0.127_darwin_arm64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
+      sha256 "ec07b4b8483791d82a6adb154f0475134913f9d36ea33af0c0aaee510ad55be7"
     end
   end
   on_linux do
     on_arm do
-      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.123/che_0.0.123_linux_arm64.tar.gz"
-      sha256 "81c06a78dbc7d4af9b486f28d4fb465c160f6363b1e907d1db2987594e63b3d5"
+      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.127/che_0.0.127_linux_arm64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
+      sha256 "da729796be23903b457d3e0c2593c0efdc7a15d117b458cb294c5f9872ecfb49"
     end
     on_intel do
-      url "https://gitlab.com/api/v4/projects/84252099/packages/generic/che/0.0.123/che_0.0.123_linux_amd64.tar.gz"
-      sha256 "abd339ec2394bb12f94cf0207f76e9a610e01437fb4cdf486e66d95d0e403244"
+      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.127/che_0.0.127_linux_amd64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
+      sha256 "5a23f4b4fa5edc8f296501640a225bfdb5c0df0f04765e30821e1f197569a197"
     end
   end
 
