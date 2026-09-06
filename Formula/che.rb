6b4ded328dddd24c1d0de27ca9529cb6b289809f
go-modules-brew-publisher <project_85188856_bot_b4e64a0e228dbaed8c7170bf97b2eccf@noreply.gitlab.com>
2026-09-06 21:28:12 +0000

    chore: che 0.0.135

--- Formula/che.rb
+++ Formula/che.rb
@@ -1,23 +1,23 @@
 class Che < Formula
   desc "Spec-driven dotfile configuration loader"
   homepage "https://gitlab.com/konradodwrot/private/che/che"
-  version "0.0.134"
+  version "0.0.135"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.134/che_0.0.134_darwin_arm64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
-      sha256 "94ce42dec2ab88c41a6d2ca74602bf3314d9e8001e2e92541b6130cc29aaa6ee"
+      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.135/che_0.0.135_darwin_arm64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
+      sha256 "d638d308d40bdabae2fe203741d8e62d1e59dc838f2614f8e127f064a99c1527"
     end
   end
   on_linux do
     on_arm do
-      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.134/che_0.0.134_linux_arm64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
-      sha256 "2849d5aed564b59eb4924d0aa0c723a62eed0a5a1e4e3400e8df789ba5feee50"
+      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.135/che_0.0.135_linux_arm64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
+      sha256 "3f847c5db3726fe12be55fa0ed59b689e2f224269c2e4af67aa72d5434105b72"
     end
     on_intel do
-      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.134/che_0.0.134_linux_amd64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
-      sha256 "5a7b4697530aea9dbf6c46f39ee33ec2a1cb6c3313dd53d2b3da7d7c918bdc1d"
+      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.135/che_0.0.135_linux_amd64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
+      sha256 "19f68802356f088bbb67986491a29b8f121518175fac111cfe10a42ab503a816"
     end
   end
 
