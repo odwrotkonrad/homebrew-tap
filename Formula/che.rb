84089de8251a2ee1d0a99a9bfdcf3ac236c0c222
konradodwrot-ci-protected-read-write <group_130724356_bot_dadc64794c30fbc58b1b03d17be79c20@noreply.gitlab.com>
2026-09-07 19:48:09 +0000

    chore: che 0.0.140

--- Formula/che.rb
+++ Formula/che.rb
@@ -1,23 +1,23 @@
 class Che < Formula
   desc "Spec-driven dotfile configuration loader"
   homepage "https://gitlab.com/konradodwrot/private/che/che"
-  version "0.0.139"
+  version "0.0.140"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.139/che_0.0.139_darwin_arm64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
-      sha256 "1076a73b5c7e30b49e02769295d2d194e3706f7a6a1a20c0c5ad134738240d5f"
+      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.140/che_0.0.140_darwin_arm64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
+      sha256 "12010a7dc0107f1d5f0833f9f1a83abc1f6adf2a5b0953fdc40d7e39fb8dcfa6"
     end
   end
   on_linux do
     on_arm do
-      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.139/che_0.0.139_linux_arm64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
-      sha256 "6352a45b7a6b38672fb9b4c745378e036436954c84b54dcccf4c1fecd4cb50b3"
+      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.140/che_0.0.140_linux_arm64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
+      sha256 "27a9c46ce1b8e4bba594b77c383e16d38b43db0702f01127abd9096dedf5eb7f"
     end
     on_intel do
-      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.139/che_0.0.139_linux_amd64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
-      sha256 "25b859fca8bb601b5cfb273a1d218f998074d7c144b7fc1296b751a94c059985"
+      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.140/che_0.0.140_linux_amd64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
+      sha256 "2ed2343314a5c5137d97e4895c3c0bfb1aa8e3cf878beca09f091d16dc6852e3"
     end
   end
 
