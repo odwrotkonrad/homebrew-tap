36632f4c3fdeef7d71109b6599c4e73a8002c667
konradodwrot-ci-protected-read-write <group_130724356_bot_dadc64794c30fbc58b1b03d17be79c20@noreply.gitlab.com>
2026-09-12 14:34:16 +0000

    chore: che 0.0.145

---
+++ Formula/che@0.0.145.rb
@@ -0,0 +1,31 @@
+class CheAT00145 < Formula
+  desc "Spec-driven dotfile configuration loader"
+  homepage "https://gitlab.com/konradodwrot/che/che"
+  version "0.0.145"
+  license "MIT"
+
+  on_macos do
+    on_arm do
+      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.145/che_0.0.145_darwin_arm64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
+      sha256 "f37dc513c55c1d406021f1392214009fecc477ffc910c2d7bb6b8a91efc52226"
+    end
+  end
+  on_linux do
+    on_arm do
+      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.145/che_0.0.145_linux_arm64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
+      sha256 "8756e0524914222c2a9c499ab3bb4fa4f308c986e46b9fa06b4a14312d86746e"
+    end
+    on_intel do
+      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.145/che_0.0.145_linux_amd64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
+      sha256 "bb9a4663e02a3d96a153a7813e4d34185561299e9da2b47102d5cb59be6283a8"
+    end
+  end
+
+  def install
+    bin.install "che"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/che --version")
+  end
+end
