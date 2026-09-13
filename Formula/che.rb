7112f3d9ff875758d2448fe65f3690eea6a9f34e
konradodwrot-ci-protected-read-write <group_130724356_bot_dadc64794c30fbc58b1b03d17be79c20@noreply.gitlab.com>
2026-09-13 18:37:58 +0000

    chore: che 0.0.154

--- Formula/che.rb
+++ Formula/che.rb
@@ -1,23 +1,23 @@
 class Che < Formula
   desc "Spec-driven dotfile configuration loader"
   homepage "https://gitlab.com/konradodwrot/che/che"
-  version "0.0.153"
+  version "0.0.154"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.153/che_0.0.153_darwin_arm64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
-      sha256 "d574ab91d21880284865fb68943c4a5aabaa5b09280fbfa6f400a3004f577c05"
+      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.154/che_0.0.154_darwin_arm64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
+      sha256 "1f6da7d21f99cf74904c20ff5a1ece5bfeefad70ea21189579a3b3ea542b1a4a"
     end
   end
   on_linux do
     on_arm do
-      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.153/che_0.0.153_linux_arm64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
-      sha256 "0c3d706c51d2383ed5169e56d8ff88ee09c7056c15652338f96a06eeaa1be57c"
+      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.154/che_0.0.154_linux_arm64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
+      sha256 "30ba2e412352327f87017aabb97f4a46acfbd62e41c9206b07f289afe0c10438"
     end
     on_intel do
-      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.153/che_0.0.153_linux_amd64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
-      sha256 "4a3821f20fb732cfc9a74cc71fc9748f8c4de7b2389a167be8012df524652907"
+      url "https://gitlab.com/api/v4/projects/86147432/packages/generic/che/0.0.154/che_0.0.154_linux_amd64.tar.gz", headers: ["PRIVATE-TOKEN: #{ENV.fetch("HOMEBREW_GITLAB_TOKEN")}"]
+      sha256 "d8030d0f06d2442f80e01b70184e0b38a85276c9e6fa235910bfd29706f3f70b"
     end
   end
 
